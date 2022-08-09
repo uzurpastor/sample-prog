156b2c034ec23694a43174d63edffd0aeb13f6d5
uzurpastor-lena <shevchenko.andrey.uzurpastor@gmail.com>
2022-08-09 12:01:02 +0300

    fix: change params for login because user fixtures was changed

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -10,7 +10,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with valid information" do 
     get login_path
-    post login_path, params: { session: { email: "example@mail.com", password: "passwd" } }
+    post login_path, params: { session: { email: "shevchenko@mail.com", password: "passwd" } }
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
