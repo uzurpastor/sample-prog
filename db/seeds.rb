2ae9f50b4bfcea87cc9a69c367e9f0b7e31b55f3
uzurpastor-lena <shevchenko.andrey.uzurpastor@gmail.com>
2022-08-11 19:08:42 +0300

    devel: create admin model

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,19 @@
   EmailActivation.create!( user_id: user.id, 
                            activation_digest: Faker::Internet.device_token )
 end
+name      = Faker::Name.first_name
+email     = Faker::Internet.free_email name: name
+password  = Faker::Internet.password min_length: 6
+user = User.create!( name:                  name,
+                     email:                 email,
+                     password:              password,
+                     password_confirmation: password )
+user
+EmailActivation.create!( user_id: user.id, 
+                         activation_digest: Faker::Internet.device_token )
 
-# User.create!(
-#   name: "Andrey",
-#   email: "shevchenko@mail.com",
-#   password: "passwd",
-#   password_confirmation: "passwd")
+User.create!(
+  name: "Andrey",
+  email: "shevchenko@mail.com",
+  password: "passwd",
+  password_confirmation: "passwd")
