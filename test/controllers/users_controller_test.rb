dde5ff02677e67d3baf279902f56d607028fe4e8
uzurpastor-lena <shevchenko.andrey.uzurpastor@gmail.com>
2022-08-14 23:05:26 +0300

    debug: pass test for users controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,10 @@
 require "test_helper"
-
+require "integration/config"
 class UsersControllerTest         < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:shevchenko)
+    @other_user = users :admin
   end
 
   test "should get sign up" do 
@@ -11,34 +12,29 @@ class UsersControllerTest         < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Sign Up | Ruby"
   end
-
-
-  ###
-  # => неадаптированный код с книги
-  ###
   
-  # test "should redirect edit when not logged in" do
-  #   get :edit, id: @user.id 
-  #   assert_not flash.empty?
-  #   assert_redirected_to login_url
-  # end
+  test "should redirect edit when not logged in" do
+    get edit_user_path @user
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
 
-  # test "should redirect update when not logged in" do
-  #   patch :update, id: @user , params: { user: { name: @user.name, email: @user.email } }
-  #   assert_not flash.empty?
-  #   assert_redirected_to login_url
-  # end
-  # test "should redirect edit when logged in as wrong user" do
-  #   log_in_as(@other_user)
-  #   get :edit, id: @user
-  #   assert flash.empty?
-  #   assert_redirected_to root_url
-  # end
+  test "should redirect update when not logged in" do
+    process :patch, "/users/#{@user.id}", params: { user: {id: @user.id, name: @user.name, email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path @user
+    assert flash.empty?
+    assert_redirected_to home_path
+  end
 
-  # test "should redirect update when logged in as wrong user" do
-  #   log_in_as(@other_user)
-  #   patch :update, id: @user, user: { name: @user.name, email: @user.email }
-  #   assert flash.empty?
-  #   assert_redirected_to root_url
-  # end
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    process :patch, "/users/#{@user.id}", params: { user: { name: @user.name, email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to home_path
+  end
 end
